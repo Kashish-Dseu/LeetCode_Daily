d5db7d907c76cd9aa08ee3835855f11d0b7a52a2
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-06-27 13:02:21 +0530

    Create 3020._Find_the_Maximum_Number_of_Elements_in_Subset.cpp

---
+++ 3020._Find_the_Maximum_Number_of_Elements_in_Subset.cpp
@@ -0,0 +1,36 @@
+/*
+Problem Name: 3020. Find the Maximum Number of Elements in Subset 
+Difficulty: Medium
+Date: 27/06/2026
+*/
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<long long, int> freq;
+        for (int x : nums) freq[x]++;
+        int ans = 1;
+        if (freq.count(1)) {
+            int c = freq[1];
+            ans = max(ans, (c%2 == 1) ? c : c - 1);
+        }
+        for (auto &[start, c] : freq) {
+            if (start == 1) continue;
+            long long cur = start;
+            int len = 0;
+            while (freq.count(cur)) {
+                if (freq[cur] >= 2) {
+                    len += 2;
+                    cur = cur * cur;
+                }
+                else {
+                    len++;
+                    break;
+                }
+            }
+            if (len % 2 == 0) len--;
+            ans = max(ans, len);
+        }
+        return ans;
+    }
+};
