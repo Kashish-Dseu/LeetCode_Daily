b169bcb47df960b31a31853324e04b7ff8279cf4
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-06-30 11:59:15 +0530

    Create 1358._Number_of_Substrings_Containing_All_Three_Characters.cpp

---
+++ 1358._Number_of_Substrings_Containing_All_Three_Characters.cpp
@@ -0,0 +1,25 @@
+/*
+Problem Name: 1358. Number of Substrings Containing All Three Characters 
+Difficulty: Medium
+Date: 30/06/2026
+*/
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        int i=0,j=0;
+        int ans=0;
+        unordered_map<char,int>mp;
+        while(j<s.size()){
+            mp[s[j]]++;
+            while(mp.size()==3){
+                ans+=(n-j);
+                mp[s[i]]--;
+                if(mp[s[i]]==0) mp.erase(s[i]);
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
