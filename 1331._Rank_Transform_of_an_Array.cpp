b5d80232e18fa71bd7967a567c8e26f70a8501ac
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-07-12 13:16:42 +0530

    Create 1331._Rank_Transform_of_an_Array.cpp

---
+++ 1331._Rank_Transform_of_an_Array.cpp
@@ -0,0 +1,18 @@
+/*
+Problem Name: 1331. Rank Transform of an Array  
+Difficulty: Easy
+Date: 12/07/2026
+*/
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n= arr.size();
+        vector<int> temp = arr;
+        sort(temp.begin(), temp.end());
+        temp.erase(unique(temp.begin(), temp.end()), temp.end());
+        for (int i = 0; i < n; i++) {
+            arr[i] = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin()+1;
+        }
+        return arr;
+    }
+};
