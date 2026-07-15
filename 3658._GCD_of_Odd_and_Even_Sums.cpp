54aeb28a5014a166b0ec5433ea8b486a181cf2d3
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-07-15 19:21:14 +0530

    Create 3658._GCD_of_Odd_and_Even_Sums.cpp

---
+++ 3658._GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,11 @@
+/*
+Problem Name: 3658. GCD of Odd and Even Sums  
+Difficulty: Easy
+Date: 15/07/2026
+*/
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
