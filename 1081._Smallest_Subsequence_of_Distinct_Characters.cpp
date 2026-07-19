651e430cbdbd3f1c4a1c7bdf9ba7229c3d071531
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-07-19 21:29:03 +0530

    Create 1081._Smallest_Subsequence_of_Distinct_Characters.cpp

---
+++ 1081._Smallest_Subsequence_of_Distinct_Characters.cpp
@@ -0,0 +1,27 @@
+/*
+Problem Name: 1081. Smallest Subsequence of Distinct Characters  
+Difficulty: Medium
+Date: 19/07/2026
+*/
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int>freq(26,0);
+        vector<bool>inStack(26,false);
+        for(char c:s)
+            freq[c-'a']++;
+        string st;
+        for(char c:s){
+            freq[c-'a']--;
+            if(inStack[c-'a'])
+                continue;
+            while(!st.empty()&&st.back()>c&&freq[st.back()-'a']>0){
+                inStack[st.back()-'a']=false;
+                st.pop_back();
+            }
+            st.push_back(c);
+            inStack[c-'a']=true;
+        }
+        return st;
+    }
+};
