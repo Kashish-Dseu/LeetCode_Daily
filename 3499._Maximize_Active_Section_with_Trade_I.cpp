587a7d6896627205877d6f71d8225f697f262277
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-07-21 19:51:18 +0530

    Create 3499._Maximize_Active_Section_with_Trade_I.cpp

---
+++ 3499._Maximize_Active_Section_with_Trade_I.cpp
@@ -0,0 +1,21 @@
+/*
+Problem Name: 3499. Maximize Active Section with Trade I 
+Difficulty: Medium
+Date: 21/07/2026
+*/
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<int> z;
+        int run = 0;
+        int ans = 0;
+        for (char c : s)
+            if(c == '0') ++run;
+            else if(run) z.push_back(exchange(run, 0));
+        if (run) z.push_back(run);
+        for (int i = 1; i < z.size(); ++i)
+            ans = max(ans, z[i - 1] + z[i]);
+
+        return ranges::count(s, '1') + ans;
+    }
+};
