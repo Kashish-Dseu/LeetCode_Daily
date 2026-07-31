99aec8f5a843f37dadcae98e7adae3f80f41af5a
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-07-31 19:31:09 +0530

    Create 3016._Minimum_Number_of_Pushes_to_Type_Word_II.cpp

---
+++ 3016._Minimum_Number_of_Pushes_to_Type_Word_II.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Name: 3016. Minimum Number of Pushes to Type Word II 
+Difficulty: Medium
+Date: 31/07/2026
+*/
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        int ans = 0;
+        for(char ch : word) freq[ch - 'a']++;
+        sort(freq.begin(), freq.end());
+        for(int i = 25, j = 0; i >= 0; i--) {
+            int curr = 25 - i;
+            if(curr % 8 == 0) j++;
+            ans += freq[i] * j;
+        }
+        return ans;
+    }
+};
