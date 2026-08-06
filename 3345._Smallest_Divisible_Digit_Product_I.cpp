93f2885ab7406ffd4c6abeaf68b3ef154eb02794
Kashish Tilwani <kashishtilwani07@gmail.com>
2026-08-06 21:08:34 +0530

    Create 3345._Smallest_Divisible_Digit_Product_I.cpp

---
+++ 3345._Smallest_Divisible_Digit_Product_I.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Name: 3345. Smallest Divisible Digit Product I  
+Difficulty: Easy
+Date: 06/08/2026
+*/
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        for (int i = n; ; ++i) {
+            int prod = 1;
+            int temp = i;
+            
+            while (temp > 0) {
+                prod *= (temp % 10);
+                temp /= 10;
+            }
+            
+            if (prod % t == 0) {
+                return i;
+            }
+        }
+    }
+};
